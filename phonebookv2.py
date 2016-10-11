5929612c6e0b74ba027f8b3289f819b236d5eb68
Juan A Cortes <juan.cortes@gmail.com>
2016-10-11 11:03:37 -0400

    Remove unnecesary save_entries() function and corresponding menu information

--- phonebookv2.py
+++ phonebookv2.py
@@ -12,33 +12,27 @@ def menu():
     2\. Set an entry
     3\. Delete an entry
     4\. List all entries
-    5\. Save entries
-    6\. Quit
+    5\. Quit
+
     What do you want to do (1-5)?
     '''
     answer = int(raw_input("> "))
     return answer
 
-def lookup_entry():
-
+# def lookup_entry():
 
 def set_entry():
         name = raw_input("Provide person's name: ").capitalize()
         phone_number = raw_input("Provide person's phone number: ")
         email = raw_input("Provide person's email: ")
         set_person = db.insert('phonebook', name = name, phone_number = phone_number, email = email);
-        print "Entry saved for %s." % (name)
+        print "Entry saved for %s." % name
 
-def delete_entry():
+# def delete_entry():
 
+# def list_all_entries():
 
-def list_all_entries():
-
-
-def save_entries():
-
-
-def quit():
+# def quit():
     # Not necessary to do this in a function, but did it to practice
 
 is_using = True
@@ -56,8 +50,6 @@ while is_using == True:
     elif choice == 4:
         list_all_entries()
     elif choice == 5:
-        save_entries()
-    elif choice == 6:
         is_using = quit() # Not necessary to do this in a function, but did it to practice
     else:
         print "Please choose a number from 1 to 5!"
